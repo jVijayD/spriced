339410d05e294ee8fbf49199eca052c15dc072c6
shabeeb-bs <shabeeb@yoonify.io>
2023-06-14 12:32:33 +0530

    -adder Cors

--- sim-spriced-api-parent/sim-spriced-defnition-api/src/main/java/com/sim/spriced/defnition/api/controllers/EntityController.java
+++ sim-spriced-api-parent/sim-spriced-defnition-api/src/main/java/com/sim/spriced/defnition/api/controllers/EntityController.java
@@ -3,6 +3,7 @@ package com.sim.spriced.defnition.api.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.sim.spriced.framework.models.EntityDefnition;
 
 import io.micrometer.core.annotation.Timed;
 
+@CrossOrigin(origins = "*")
 @RestController()
 @RequestMapping("/entities")
 public class EntityController {
