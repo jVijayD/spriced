7fb8c8044f6a9fd608264a4f8927f274ee757a0e
anujoo <anu.johny@simadvisorypartner.com>
2023-07-25 12:39:29 +0530

    autguard

--- apps/spriced-container/src/app/app.routes.ts
+++ apps/spriced-container/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { loadRemoteModule } from "@nx/angular/mf";
 import { HomeComponent } from "./pages/home/home.component";
 import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found/page-not-found.component";
 import { LandingPageComponent } from "./pages/landingPage/landing-page.component";
-
+import { AuthGuard } from "@spriced-frontend/shared/spriced-shared-lib";
 export const appRoutes: Route[] = [
   {
     path: "spriced-user-management",
@@ -11,7 +11,7 @@ export const appRoutes: Route[] = [
       loadRemoteModule("spriced-user-management", "./Routes").then(
         (m) => m.remoteRoutes
       ),
-  
+    canActivate: [AuthGuard],
   },
   {
     path: "spriced-reports",
@@ -19,13 +19,13 @@ export const appRoutes: Route[] = [
       loadRemoteModule("spriced-reports", "./Routes").then(
         (m) => m.remoteRoutes
       ),
-  
+    canActivate: [AuthGuard],
   },
   {
     path: "spriced-data",
     loadChildren: () =>
       loadRemoteModule("spriced-data", "./Routes").then((m) => m.remoteRoutes),
-  
+    canActivate: [AuthGuard],
   },
   {
     path: "spriced-data-definition",
@@ -33,17 +33,15 @@ export const appRoutes: Route[] = [
       loadRemoteModule("spriced-data-definition", "./Routes").then(
         (m) => m.remoteRoutes
       ),
-  
+    canActivate: [AuthGuard],
   },
   {
     path: "home",
     component: HomeComponent,
-  
   },
   {
     path: "",
     component: LandingPageComponent,
-  
   },
   {
     path: "**",
--- libs/shared/spriced-shared-lib/src/index.ts
+++ libs/shared/spriced-shared-lib/src/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/shared-spriced-shared-lib.module";
 
 export * from "./lib/auth/keycloak-init.factory";
 export * from "./lib/auth/keycloak-bearer.Interceptor";
+export * from "./lib/auth/auth.guard";
