db21446642625b57e9690e31bc3c7f453e0e4219
Arjun M <arjun.meleppatt@simadvisorypartner.com>
2023-08-04 12:11:13 +0530

    code changes

--- sim-spriced-data/src/main/java/com/sim/spriced/data/service/IBulkUploadService.java
+++ sim-spriced-data/src/main/java/com/sim/spriced/data/service/IBulkUploadService.java
@@ -1,11 +1,10 @@
 package com.sim.spriced.data.service;
 
+import com.sim.spriced.framework.models.BulkEntityDetails;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.sim.spriced.framework.models.FileEntity;
+public interface IBulkUploadService {
 
-public interface IFileService {
-
-	public FileEntity uploadFileDetails(FileEntity fileDetails, MultipartFile file);
+	public BulkEntityDetails uploadFileDetails(BulkEntityDetails bulkEntityDetails, MultipartFile file);
 
 }
