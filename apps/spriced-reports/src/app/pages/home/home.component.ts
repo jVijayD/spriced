d72f93c062e1789c0d6640dfdbf8ce9cf71687a8
shabeeb-bs <shabeeb@yoonify.io>
2023-08-08 18:05:20 +0530

    reports in progress

--- apps/spriced-reports/src/app/pages/home/home.component.ts
+++ apps/spriced-reports/src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { embedDashboard } from "@superset-ui/embedded-sdk";
 import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "sp-home",
@@ -12,12 +13,14 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   //dashboardId:string="";
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {}
   ngOnInit(): void {
     let dashboardId = this.route.snapshot.paramMap.get("dashboardId");
     if (!dashboardId) {
       dashboardId = "f4adaabb-7e11-470a-b9e8-0f65964f80ee";
     }
+
+    this.getTokens();
     // embedDashboard({
     //   id: "abc123", // given by the Superset embedding UI
     //   supersetDomain: "https://superset.example.com",
@@ -35,5 +38,13 @@ export class HomeComponent implements OnInit {
     //     },
     //   },
     // });
+  }
+
+  private setDashboard() {
+    const login = {};
+    this.httpClient.post<any>(
+      "http://localhost:8787/api/v1/security/login",
+      login
+    );
   }
 }
