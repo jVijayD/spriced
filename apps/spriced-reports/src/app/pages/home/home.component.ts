726799d2c2adc6b9a5ccc964ced67b16b0c643cf
shabeeb-bs <shabeeb@yoonify.io>
2023-08-08 10:45:38 +0530

    added the dashboard ids

--- apps/spriced-reports/src/app/pages/home/home.component.ts
+++ apps/spriced-reports/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { embedDashboard } from "@superset-ui/embedded-sdk";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "sp-home",
@@ -10,23 +11,29 @@ import { embedDashboard } from "@superset-ui/embedded-sdk";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  //dashboardId:string="";
+  constructor(private route: ActivatedRoute) {}
   ngOnInit(): void {
-    embedDashboard({
-      id: "abc123", // given by the Superset embedding UI
-      supersetDomain: "https://superset.example.com",
-      mountPoint: document.getElementById(
-        "my-superset-container"
-      ) as HTMLElement, // any html element that can contain an iframe
-      fetchGuestToken: () => {
-        return Promise.resolve("");
-      },
-      dashboardUiConfig: {
-        // dashboard UI config: hideTitle, hideTab, hideChartControls, filters.visible, filters.expanded (optional)
-        hideTitle: true,
-        filters: {
-          expanded: true,
-        },
-      },
-    });
+    let dashboardId = this.route.snapshot.paramMap.get("dashboardId");
+    if (!dashboardId) {
+      dashboardId = "f4adaabb-7e11-470a-b9e8-0f65964f80ee";
+    }
+    // embedDashboard({
+    //   id: "abc123", // given by the Superset embedding UI
+    //   supersetDomain: "https://superset.example.com",
+    //   mountPoint: document.getElementById(
+    //     "my-superset-container"
+    //   ) as HTMLElement, // any html element that can contain an iframe
+    //   fetchGuestToken: () => {
+    //     return Promise.resolve("");
+    //   },
+    //   dashboardUiConfig: {
+    //     // dashboard UI config: hideTitle, hideTab, hideChartControls, filters.visible, filters.expanded (optional)
+    //     hideTitle: true,
+    //     filters: {
+    //       expanded: true,
+    //     },
+    //   },
+    // });
   }
 }
