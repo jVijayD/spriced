1c6f1dcc03861e9d9d287c762f47cd731f711762
mayur <mayurkolhe26>
2023-08-30 22:46:43 +0530

    change_the_value

--- apps/spriced-data-definition/src/app/pages/model/components/model-add/model-add.component.ts
+++ apps/spriced-data-definition/src/app/pages/model/components/model-add/model-add.component.ts
@@ -46,6 +46,9 @@ export class ModelAddComponent {
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.dialogRef.disableClose = true;
+    if(this.data.action == "Add" || this.data.action === "Edit"){
+      this.dialogRef.disableClose = false;
+    }
     this.appForm = {
       title: this.data.action === "Edit" ? "Edit Model" : "Add Model",
       //columns: 4,
