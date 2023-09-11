05e92ef95e79d16e0586e18764e1ec06830bee02
shabeeb-bs <shabeeb@yoonify.io>
2023-09-11 19:19:06 +0530

    added report and user management

--- apps/spriced-user-management/src/app/remote-entry/entry.routes.ts
+++ apps/spriced-user-management/src/app/remote-entry/entry.routes.ts
@@ -1,6 +1,15 @@
-import { Route } from '@angular/router';
-import { RemoteEntryComponent } from './entry.component';
+import { Route } from "@angular/router";
+import { AppComponent } from "../app.component";
+import { HomeComponent } from "../pages/home/home.component";
 
 export const remoteRoutes: Route[] = [
-  { path: '', component: RemoteEntryComponent },
+  {
+    path: "",
+    component: AppComponent,
+    children: [
+      { path: "", component: HomeComponent },
+      { path: "item2", component: HomeComponent },
+      { path: "item3", component: HomeComponent },
+    ],
+  },
 ];
--- apps/spriced-user-management/src/bootstrap.ts
+++ apps/spriced-user-management/src/bootstrap.ts
@@ -1,11 +1,11 @@
 import { importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { RemoteEntryComponent } from "./app/remote-entry/entry.component";
 import { appRoutes } from "./app/app.routes";
 import { SharedSpricedSharedLibModule } from "@spriced-frontend/shared/spriced-shared-lib";
+import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(RemoteEntryComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(SharedSpricedSharedLibModule),
     importProvidersFrom(
