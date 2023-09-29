18120733754225e804be26cc0042e966fee53060
Ajay <ajay.choudhary@simadvisorypartner.com>
2023-09-29 14:10:10 +0530

    quick fix rule form

--- apps/spriced-data-definition/src/app/business-rules/business-rule-name/business-rule-name.component.ts
+++ apps/spriced-data-definition/src/app/business-rules/business-rule-name/business-rule-name.component.ts
@@ -426,7 +426,7 @@ export class BusinessRuleNameComponent implements OnInit, OnDestroy {
       attributeId: item?.attributeId ? [item.attributeId, Validators.required] : ['', Validators.required],
       actionType: item?.actionType ? [item.actionType, Validators.required] : ['', Validators.required],
       operatorType: item?.operatorType ? [item.operatorType, Validators.required] : ['', Validators.required],
-      operand: item?.operand && !item?.operand?.max ? [item.operand, Validators.required] : ['', Validators.required],
+      operand: item?.operand.toString() ? [item.operand, Validators.required] : ['', Validators.required],
       max_value: value && value[1] ? [value[1], Validators.required] : ['', Validators.required],
       min_value: value && value[0] ? [value[0], Validators.required] : ['', Validators.required],
       operandType: item?.operandType ? [item.operandType, Validators.required] : ['ATTRIBUTE', Validators.required],
