0b43f8afa3dfce080ce7f1fa5e9f49ef2afa0384
Deepak <deepak.jumpstart@simadvisorypartner.com>
2023-10-04 10:14:45 +0530

    save button enabled when user change any value in edit entity

--- apps/spriced-data-definition/src/app/pages/entity/components/entity-add/entity-add.component.ts
+++ apps/spriced-data-definition/src/app/pages/entity/components/entity-add/entity-add.component.ts
@@ -115,6 +115,8 @@ export class EntityAddComponent implements OnInit {
   isFullScreen = false;
   totalElements = 0;
   selectedItem: any = null;
+  isChangedValue: Boolean = true
+  previousEntityData: any;
   systemAtt: any;
   @ViewChild(DataGridComponent)
   dataGrid!: DataGridComponent;
@@ -133,6 +135,7 @@ export class EntityAddComponent implements OnInit {
     this.attDetails.type = "FREE_FORM";
     this.attDetails.width = DEFAULT_ATTRIBUTE_WIDTH;
     this.local_data = { ...data.row };
+    this.previousEntityData = { ...data.row };
     this.entityList = data.entities;
 
     if (this.local_data.id == this.entityList[0]?.id) {
@@ -173,12 +176,8 @@ export class EntityAddComponent implements OnInit {
       }
     );
 
-    this.local_data.attributes = this.local_data?.attributes?.filter(
-      (value: any) => {
-        return !value.systemAttribute;
-      }
-    );
-
+    this.filterAttributes(this.local_data);
+    this.filterAttributes(this.previousEntityData);
 
     // if (this.local_data?.attributes) {
     //   this.local_data?.attributes.forEach((attribute: any) => {
@@ -196,6 +195,20 @@ export class EntityAddComponent implements OnInit {
   ngOnInit(): void {
     this.initForm();
   }
+
+  /**
+   * HANDLE THIS FUNCTION FOR FILTER THE SYSTEM ATTRIBUTES
+   * @param item any
+   */
+  public filterAttributes(item: any)
+  {
+    item.attributes = item?.attributes?.filter(
+      (value: any) => {
+        return !value.systemAttribute;
+      }
+    );
+  }
+
   doAction() {
     console.log(this.rows)
     this.local_data.attributes = [...this.rows];
@@ -207,6 +220,45 @@ export class EntityAddComponent implements OnInit {
     }
     this.dataChange.emit(this.local_data);
   }
+
+  public modelChangedValue(item: any)
+  {
+    this.isChangedValue = this.areObjectsEqual(this.previousEntityData, this.local_data);
+  }
+
+  /**
+ * HANDLE THIS FUNCTION FOR MATCHING THE TWO OBJECTS
+ * @param obj1 any
+ * @param obj2 any
+ * @returns 
+ */
+  public areObjectsEqual(obj1: any, obj2: any): boolean {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (let key of keys1) {
+      const val1 = obj1[key];
+      const val2 = obj2[key];
+  
+      // Check if the values are objects and recursively compare them
+      if (typeof val1 === 'object' && val1 !== null && typeof val2 === 'object' && val2 !== null) {
+        if (!this.areObjectsEqual(val1, val2)) {
+          return false;
+        }
+      } else if (val1 !== val2) {
+        // Values are not equal
+        return false;
+      }
+    }
+  
+    // All keys and values are equal
+    return true;
+  }
+
 
   closeDialog() {
     this.dialogRef.close({ event: "Cancel" });
@@ -287,6 +339,7 @@ export class EntityAddComponent implements OnInit {
       this.rows = [...this.rows];
       this.totalElements = this.rows.length;
     }
+    this.isChangedValue = this.areObjectsEqual(this.previousEntityData.attributes, this.rows);
     this.clear();
   }
 
@@ -325,6 +378,7 @@ export class EntityAddComponent implements OnInit {
         this.rows = this.rows.filter((value: any) => {
           return value.name != this.selectedItem.name;
         });
+        this.isChangedValue = this.areObjectsEqual(this.previousEntityData.attributes, this.rows);
         this.clear();
       }
     })
