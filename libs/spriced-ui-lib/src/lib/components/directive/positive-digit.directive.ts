5e132a527c6a2bd3e4f8184cb1fbdcbab19153cd
mayur <mayurkolhe26>
2023-10-13 16:21:23 +0530

    using not operator

--- libs/spriced-ui-lib/src/lib/components/directive/positive-digit.directive.ts
+++ libs/spriced-ui-lib/src/lib/components/directive/positive-digit.directive.ts
@@ -6,20 +6,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class PositiveDigitDirective {
   constructor(private el:ElementRef) { }
-  allowedKeypress = [8,13,37,39,46,38,40];
+  allowedKeypress = [8,13,37,39,38,40];
   @Input() public SelectedValue:number=1;
-  @HostListener('keydown',['$event'])
+  @HostListener('keypress',['$event'])
    
   onKeyDown(e:any){
-      if ((this.allowedKeypress.includes(e.which))||(e.which > 47 && e.which < 58 && e.target.value.length < this.SelectedValue)) 
-      {
-          //if (e.target.value < 0 || e.target.value > 99) {
-               // e.preventDefault();
-              //}
-      } 
-      else {
-        e.preventDefault();
-      }
-
+    if ((!this.allowedKeypress.includes(e.which)) && !(e.which >= 47 && e.which <= 58) || e.target.value.length >=this.SelectedValue) 
+    {
+          e.preventDefault();
     }
+  }
 }
