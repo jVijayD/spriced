7152af03bc7846696ee6409225e3a20f5214458b
sahil <sahil.jumpstart@simadvisorypartner.com>
2023-10-26 12:16:13 +0530

    added filters for viewTransactoin

--- apps/spriced-data-definition/src/app/pages/view-Transactions/viewTransactions-admin/view-transactions-admin.component.ts
+++ apps/spriced-data-definition/src/app/pages/view-Transactions/viewTransactions-admin/view-transactions-admin.component.ts
@@ -2,10 +2,13 @@ import { Component, Inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
   DataGridComponent,
+  DialogService,
+  DialogueModule,
   Header,
   HeaderActionComponent,
   HeaderComponentWrapperComponent,
   Paginate,
+  QueryColumns,
 } from "@spriced-frontend/spriced-ui-lib";
 import {
   ColumnMode,
@@ -28,13 +31,16 @@ import {
   EntityService,
 } from "@spriced-frontend/spriced-common-lib";
 import { ModelService } from "../../../services/model.service";
-import { forkJoin } from "rxjs";
+import { Subject, forkJoin } from "rxjs";
+import { NgxMatSelectSearchModule } from "ngx-mat-select-search";
+import { MatToolbarModule } from "@angular/material/toolbar";
 @Component({
   selector: "sp-view-transactions-admin",
   standalone: true,
   imports: [
     CommonModule,
     DataGridComponent,
+    MatToolbarModule,
     HeaderComponentWrapperComponent,
     MatFormFieldModule,
     MatIconModule,
@@ -42,9 +48,12 @@ import { forkJoin } from "rxjs";
     MatTableModule,
     MatInputModule,
     MatSelectModule,
+    DialogueModule,
     HeaderActionComponent,
+    NgxMatSelectSearchModule,
     ToolTipRendererDirective,
   ],
+  providers: [DialogService],
   templateUrl: "./view-transactions-admin.component.html",
   styleUrls: ["./view-transactions-admin.component.scss"],
 })
@@ -55,12 +64,15 @@ export class ViewTransactionsAdminComponent {
   columnMode: ColumnMode = ColumnMode.force;
   selectionType: SelectionType = SelectionType.single;
   totalElements = 0;
+  public showTooltip: boolean = false;
   rows: any[] = [];
+  filters:any;
   limit: number = 10;
   modelList!: any[];
   entityList!: any[];
   query?: any;
   subscriptions: any[] = [];
+  pageNumber!:number;
   currentCriteria: Criteria = {
     filters: [],
     pager: {
@@ -71,7 +83,8 @@ export class ViewTransactionsAdminComponent {
   constructor(
     private modelService: ModelService,
     private enitityService: EntityService,
-    private transactionService: TransactionsService
+    private transactionService: TransactionsService,
+    private dialogService:DialogService,
   ) {}
   headers: Header[] = [
     {
@@ -134,6 +147,7 @@ export class ViewTransactionsAdminComponent {
     this.loadEntitiesById(this.selectedModel);
   }
   onPaginate(e: Paginate) {
+    this.pageNumber = e.offset;
     if (
       this.currentCriteria.filters &&
       this.currentCriteria.filters.length > 0 &&
@@ -196,6 +210,121 @@ export class ViewTransactionsAdminComponent {
       (error: any) => {}
     );
   }
+
+  onFilter(){
+    debugger
+    const dialogResult = this.dialogService.openFilterDialog({
+      persistValueOnFieldChange: true,
+      columns:this.getFilterColumns(this.headers),
+      emptyMessage: "Please select filter criteria.",
+      config: null,
+      query: this.query,
+    });
+    dialogResult.afterClosed().subscribe((val) => {
+      this.query = dialogResult.componentInstance.data.query;
+      this.addDisplayNameInFilter(this.query);
+      this.currentCriteria.filters = val;
+      // this.loadTransactionsData(modelId,this.currentCriteria)
+    });
+   }
+
+   getFilterColumns(headers: Header[]): QueryColumns[] {
+    return headers
+      .map((col: any) => {
+        return {
+          name: col.column,
+          displayName: col.name,
+          dataType:  "string",
+          options: col?.dataType === "category" ? col.options : undefined,
+        };
+      });
+  }
+
+
+  onClearFilter(){
+    this.query = null;
+    this.currentCriteria.filters = [];
+    // this.loadTransactionsData(modelId,this.currentCriteria)
+   }
+   /**
+   * HANDLE THIS FUNCTION FOR ADD DISPLAY NAME IN FILTER QUERY
+   * @param query any
+   */
+  public addDisplayNameInFilter(query: any) {
+    if (query.rules) {
+      query.rules.forEach((el: any) => {
+        const item: any = this.headers.find(
+          (elm: any) => elm.column === el.field
+        );
+        if (el?.rules && el?.rules.length > 0) {
+          this.addDisplayNameInFilter(el); // Recursively process sub-rules
+        }
+        if (!!item) {
+          el.displayName = item.name;
+        }
+        return;
+      });
+    }
+  }
+
+   public getToolTipTemplate(conditions: any): string {
+    this.showTooltip = !!conditions;
+    if (!conditions || conditions.length === 0) {
+      return "";
+    }
+    const text: any = this.getTooltipText(conditions);
+    return text;
+  }
+
+  public getTooltipText(items: any): string {
+    let tooltipText = "";
+    if (items.condition && items.rules && items.rules.length > 0) {
+      const lastItem = items.rules.length - 1;
+      items.rules.forEach((rule: any, index: number) => {
+        if (!rule.condition && !rule.rules) {
+          const field = rule?.displayName;
+          const value = !!rule?.value ? rule?.value : "";
+          const condition = lastItem !== index ? items.condition : "";
+          tooltipText += `<strong>${field}</strong> ${rule.operator} ${value} <strong>${condition}</strong> <br>`;
+        }
+        if (!!rule.condition && !!rule.rules) {
+          tooltipText += `(`;
+          tooltipText += this.getNestedTooltipText(rule);
+          tooltipText += "<br>";
+        }
+      });
+    } else if (items.field && items.operator && items.value) {
+      tooltipText += `${items.condition} <strong>${items.field}</strong> ${items.operator} ${items.value}`;
+    }
+    return tooltipText;
+  }
+
+  public getNestedTooltipText(items: any): string {
+    debugger
+    let tooltipText = "";
+    if (items.condition && items.rules && items.rules.length > 0) {
+      const lastItem = items.rules.length - 1;
+      items.rules.forEach((rule: any, index: number) => {
+        if (!rule.condition && !rule.rules) {
+          const field = rule?.displayName;
+          const value = !!rule?.value ? rule?.value : "";
+          const condition = lastItem !== index ? items.condition : "";
+          tooltipText += `<strong>${field}</strong> ${rule.operator} ${value} <strong>${condition}</strong>`;
+          if (index < items.rules.length - 1) {
+            tooltipText += "<br>";
+          }
+        }
+        if (!!rule.condition && !!rule.rules) {
+          tooltipText += `(`;
+          tooltipText += this.getNestedTooltipText(rule);
+        }
+      });
+    }
+    tooltipText += ")";
+    return tooltipText;
+  }
+
+
   ngOnDestroy(): void {
     this.subscriptions.forEach((item) => item.unsubscribe());
   }
