e62267998f4c388535c2c67c60430376de93915e
Deepak <deepak.jumpstart@simadvisorypartner.com>
2023-12-06 16:52:46 +0530

    Resolved the sorting bug

--- apps/spriced-data/src/app/pages/entity-data/entity-data.component.ts
+++ apps/spriced-data/src/app/pages/entity-data/entity-data.component.ts
@@ -128,6 +128,7 @@ export class EntityDataComponent implements OnDestroy, OnInit {
   currentCriteria!: Criteria;
   globalSettings!: any;
   query?: any;
+  columnSort: boolean = false;
 
   entityDataLoadCompleted$ = new Subject();
 
@@ -236,8 +237,9 @@ export class EntityDataComponent implements OnDestroy, OnInit {
       return { direction: sort.dir.toUpperCase(), property: sort.prop };
     });
 
+    this.columnSort = true;
     const criteria: Criteria = { ...this.currentCriteria, sorters: sorters };
-    this.loadEntityData(this.currentSelectedEntity as Entity, criteria, true);
+    this.loadEntityData(this.currentSelectedEntity as Entity, criteria, this.columnSort);
   }
 
   onItemSelected(e: any) {
@@ -497,6 +499,7 @@ export class EntityDataComponent implements OnDestroy, OnInit {
   onEntitySelectionChange(entity: Entity | string) {
     this.selectedItem = null;
     this.pageNumber = 0;
+    this.columnSort = false;
     this.currentSelectedEntity = undefined;
     this.dataGrid.table._internalColumns = [...[]];
     this.currentSelectedEntity = entity === "" ? undefined : (entity as Entity);
@@ -719,7 +722,7 @@ export class EntityDataComponent implements OnDestroy, OnInit {
     criteria: Criteria,
     columnSort?: boolean
   ) {
-    if (!columnSort) {
+    if (!this.columnSort) {
       const sort: any = { direction: "DESC", property: "updated_date" };
       criteria.sorters = [sort];
     }
