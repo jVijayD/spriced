ae6f82348b1d73ac702a7b376829bf5da751a070
anujoo <anu.johny@simadvisorypartner.com>
2023-12-18 17:27:00 +0530

    donloads progress bar design

---
+++ apps/spriced-container/src/app/components/downloads-progress/downloads-dialogue/downloads-dialogue.component.ts
@@ -0,0 +1,46 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderActionComponent } from "@spriced-frontend/spriced-ui-lib";
+import { DownloadsProgressComponent } from "../downloads-progress.component";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+
+@Component({
+  selector: "sp-downloads-dialogue",
+  standalone: true,
+  imports: [CommonModule, HeaderActionComponent, MatProgressBarModule],
+  templateUrl: "./downloads-dialogue.component.html",
+  styleUrls: ["./downloads-dialogue.component.scss"],
+})
+export class DownloadsDialogueComponent {
+  data=[
+    {
+      entity:"entit1",
+      value:40
+    },
+    {
+      entity:"entit1",
+      value:80
+    },
+    {
+      entity:"entit2",
+      value:100
+    },
+    {
+      entity:"entit3",
+      value:90
+    },
+    {
+      entity:"entit4",
+      value:20
+    },
+    {
+      entity:"entit5",
+      value:30
+    },
+  ]
+  constructor(public dialogRef: MatDialogRef<DownloadsDialogueComponent>) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
