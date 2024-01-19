681141183a9e98daa53f7eb851490b63d71834af
Deepak <deepak.jumpstart@simadvisorypartner.com>
2024-01-19 17:41:18 +0530

    Some change in priority validation

--- apps/spriced-data-definition/src/app/business-rules/business-rule-name/business-rule-name.component.ts
+++ apps/spriced-data-definition/src/app/business-rules/business-rule-name/business-rule-name.component.ts
@@ -121,6 +121,10 @@ export class BusinessRuleNameComponent implements OnInit, OnDestroy {
 
     const {rules} = await this.getAllRulesApi();
     this.allRules = rules.filter((el:any) => el.entityId === +this.entityId);
+    if(!!ruleId)
+    {
+      this.allRules = this.allRules.filter((el:any) => el.id !== +ruleId);
+    }
 
     this.formbuild();
 
@@ -184,7 +188,7 @@ return JSON.stringify(a)===JSON.stringify(b)
         for (const el of relatedRefreneceTableEntity) {
           const { entityData } = await this.getEntityById(el.referencedTableId);
           let filteredAttributes = entityData?.attributes.filter((attr: any) => attr.name !== 'id');
-          filteredAttributes = entityData?.attributes.filter((attr: any) => attr.name !== 'code');
+          // filteredAttributes = entityData?.attributes.filter((attr: any) => attr.name !== 'code');
           entityData.attributes = filteredAttributes.filter((attr: any) => !attr.systemAttribute);
           await this.processNestedAttributes(entityData.attributes, el);
         }
