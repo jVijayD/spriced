e3f11305e21ca6a651a71071665fa9366f54973c
Deepak <deepak.jumpstart@simadvisorypartner.com>
2024-02-16 09:38:41 +0530

    Resolved paste bug

--- libs/spriced-ui-lib/src/lib/components/dynamic-form/directives/numeric.directive.ts
+++ libs/spriced-ui-lib/src/lib/components/dynamic-form/directives/numeric.directive.ts
@@ -26,9 +26,9 @@ export class NumericDirective {
       // This regex check only numbers, decimal numbers and negative numbers.
       const validNumberRegex = new RegExp(/^-?\d*\.?\d*$/);
       const number = validNumberRegex.test(this.el.nativeElement.value);
-      this.el.nativeElement.value = !number ? oldValue : this.el.nativeElement.value;
+      this.el.nativeElement.value = !number || !this.check(this.el.nativeElement.value) ? oldValue : this.el.nativeElement.value;
       this.value.emit(this.el.nativeElement.value);
-      // const currentValue = this.el.nativeElement.value;
+      // const currentValue = this.el.nativeElement.value;  
       // if (currentValue !== "" && !this.check(currentValue)) {
       //   // this.el.nativeElement.value = oldValue;
       //   //(this.el.nativeElement as HTMLInputElement).value = oldValue;
