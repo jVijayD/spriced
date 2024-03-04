ce5959456f7180fd3b61ce42cc09b3fd7b9e720c
Deepak <deepak.jumpstart@simadvisorypartner.com>
2024-03-04 12:18:38 +0530

    I have sended the date in UTC format

--- libs/spriced-common-lib/src/lib/services/utility/moment-date-format.service.ts
+++ libs/spriced-common-lib/src/lib/services/utility/moment-date-format.service.ts
@@ -1,29 +1,18 @@
+import * as moment from "moment";
+
 export const unixTimeStamp = (timestamp: any): any => {
   // public async unixTimeStamp(timestamp: any): Promise<any> {
   try {
-    // const timestamp = 1458601200000; // Unix timestamp in milliseconds
     const selectedDate = new Date(timestamp);
-    // const currentDate = new Date();
+    const currentDate = new Date();
 
-    // selectedDate.setHours(currentDate.getHours());
-    // selectedDate.setMinutes(currentDate.getMinutes());
-    // selectedDate.setSeconds(currentDate.getSeconds());
+    selectedDate.setHours(currentDate.getHours());
+    selectedDate.setMinutes(currentDate.getMinutes());
+    selectedDate.setSeconds(currentDate.getSeconds());
 
-    // const dateWithTimeStamp: any = moment(selectedDate);
-    // const date = new Date(dateWithTimeStamp);
-
-    // Get the components of the date (year, month, day, etc.)
-    const year = selectedDate.getFullYear();
-    const month = selectedDate.getMonth() + 1; // Months are zero-based, so add 1
-    const day = selectedDate.getDate();
-    const hours = selectedDate.getHours();
-    const minutes = selectedDate.getMinutes();
-    const seconds = selectedDate.getSeconds();
-
-    // Create a formatted date string
-    const formattedDate = `${year}-${pad(month)}-${pad(
-      day
-    )} ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    const dateWithTimeStamp: any = moment(selectedDate);
+    const date = new Date(dateWithTimeStamp);
+    const formattedDate = date.toISOString();
     return formattedDate;
   } catch (error) {
     console.log(error);
